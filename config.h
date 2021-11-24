2187bcf07c8a7d45242cda7794293e5fb3ef8324
Max Petretta <git@maxpetretta.com>
2021-11-23 22:41:48 -0500

    Saved firmware space w/ link time optimization

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Settings for easy combos, see: http://combos.gboards.ca/docs/install/
-#define COMBO_TERM 50
+#define COMBO_TERM 40
 #define COMBO_VARIABLE_LEN
 #define QMK_KEYS_PER_SCAN 4
 
@@ -10,11 +10,6 @@
 #define TAPPING_FORCE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 
-// Toggle layers with a triple tap
-// #define TAPPING_TOGGLE 3
-
-#define MIDI_BASIC
-
-#ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(PLANCK_SOUND)
-#endif
+// Disable unneeded features
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
--- rules.mk
+++ rules.mk
@@ -1,5 +1,8 @@
 # Enable combos and media keys
-SRC += muse.c
 VPATH += keyboards/gboards/
 COMBO_ENABLE = yes
 EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
+
+# Link time optimization
+EXTRAFLAGS += -flto
